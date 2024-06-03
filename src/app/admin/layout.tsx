8e24cfc5b491e131864d7d6c80657c86c5c82ce6
Eren Yalçın <yalcineren97@gmail.com>
2024-06-03 16:38:32 +0300

    feat(admin)
    - setup dashboard cards
    - use prisma for async data
    - add loading state for admin

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,7 @@
 import { Nav, NavLink } from "@/component/nav/Nav";
 
+export const dynamic = "force-dynamic";
+
 export default function AdminLayout({
   children,
 }: Readonly<{
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,108 @@
-export default function AdminDashboard() {
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/component/ui/card";
+import prisma from "@/core/db/db";
+import { formatCurrency, formatNumber } from "@/core/util/format/formatUtils";
+
+async function getSalesData() {
+  const data = await prisma.order.aggregate({
+    _sum: {
+      priceInCents: true,
+    },
+    _count: true,
+  });
+
+  return {
+    amount: (data._sum.priceInCents || 0 / 100) / 100,
+    numberOfSales: data._count,
+  };
+}
+
+async function getUserData() {
+  const [userCount, orderData] = await Promise.all([
+    prisma.user.count(),
+    prisma.order.aggregate({
+      _sum: {
+        priceInCents: true,
+      },
+    }),
+  ]);
+
+  return {
+    userCount,
+    averageValuePerUser:
+      userCount === 0 ? 0 : orderData._sum.priceInCents || 0 / userCount / 100,
+  };
+}
+
+async function getProductData() {
+  const [numOfAvailableProducts, numOfUnavailableProducts] = await Promise.all([
+    prisma.product.count({
+      where: {
+        isAvailableForPurchase: true,
+      },
+    }),
+    prisma.product.count({
+      where: {
+        isAvailableForPurchase: false,
+      },
+    }),
+  ]);
+
+  return {
+    numOfAvailableProducts,
+    numOfUnavailableProducts,
+  };
+}
+
+export default async function AdminDashboard() {
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData(),
+  ]);
+  const { amount: totalAmountOfSales, numberOfSales } = salesData;
+  const { userCount, averageValuePerUser } = userData;
+
   return (
-    <div>
-      <h1 className={"text-primary"}>{"Dashboard"}</h1>
+    <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"}>
+      <DashboardCard
+        title={"Sales"}
+        subtitle={`Number of sales: ${formatNumber(numberOfSales)}`}
+        body={`Total amount of sales: ${formatCurrency(totalAmountOfSales)}`}
+      />
+      <DashboardCard
+        title={"Users"}
+        subtitle={`Average value per user ${formatCurrency(averageValuePerUser)}`}
+        body={`Total amount of user: ${formatNumber(userCount)}`}
+      />
+      <DashboardCard
+        title={"Produts"}
+        subtitle={`Available products: ${formatNumber(productData.numOfAvailableProducts)}`}
+        body={`Unavailable products: ${formatNumber(productData.numOfUnavailableProducts)}`}
+      />
     </div>
+  );
+}
+
+type DashboardCardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>{body}</CardContent>
+    </Card>
   );
 }
