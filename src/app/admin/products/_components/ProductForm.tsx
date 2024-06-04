eb2c43e5b14da34db1d3866c8e0f8780292200f3
Eren Yalçın <yalcineren97@gmail.com>
2024-06-04 19:45:48 +0300

    setup(product/new): create initial ui for new product form

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -1,3 +1,52 @@
+"use client";
+
+import { useState } from "react";
+
+import { Button } from "@/component/ui/button";
+import { Input } from "@/component/ui/input";
+import { Label } from "@/component/ui/label";
+import { Textarea } from "@/component/ui/textarea";
+import { formatCurrency } from "@/core/util/format/formatUtils";
+
+import { addProduct } from "../../_actions/product";
+
 export default function ProductForm() {
-  return <form className={"mt-4"}>{"Product Form"}</form>;
+  const [priceInCents, setPriceInCents] = useState<number>();
+
+  return (
+    <form action={addProduct} className={"mt-4 space-y-8"}>
+      <div className={"space-y-2"}>
+        <Label htmlFor={"name"}>{"Name"}</Label>
+        <Input type={"text"} id={"name"} name={"name"} required />
+      </div>
+      <div className={"space-y-2"}>
+        <Label htmlFor={"priceInCents"}>{"Price in cents"}</Label>
+        <Input
+          type={"number"}
+          id={"priceInCents"}
+          name={"priceInCents"}
+          value={priceInCents}
+          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
+          required
+        />
+        <div className={"text-muted-foreground"}>
+          {formatCurrency((priceInCents || 0) / 100)}
+        </div>
+      </div>
+      <div className={"space-y-2"}>
+        <Label htmlFor={"description"}>{"Description"}</Label>
+        <Textarea id={"description"} name={"description"} required />
+      </div>
+      <div className={"space-y-2"}>
+        <Label htmlFor={"file"}>{"File"}</Label>
+        <Input type={"file"} id={"file"} name={"file"} required />
+      </div>
+      <div className={"space-y-2"}>
+        <Label htmlFor={"image"}>{"Image"}</Label>
+        <Input type={"file"} id={"image"} name={"image"} required />
+      </div>
+
+      <Button type={"submit"}>{"Save"}</Button>
+    </form>
+  );
 }
