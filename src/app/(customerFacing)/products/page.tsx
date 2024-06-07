6a48b7400dbbbf68e06a4800479b2cd7a0ad00f0
Eren Yalçın <yalcineren97@gmail.com>
2024-06-07 23:52:03 +0300

    feat(products): implement products page

---
+++ src/app/(customerFacing)/products/page.tsx
@@ -0,0 +1,44 @@
+import { Suspense } from "react";
+
+import {
+  ProductCard,
+  ProductCardSkeleton,
+} from "@/component/product-card/ProductCard";
+import prisma from "@/core/db/db";
+
+const getProducts = async () => {
+  return prisma.product.findMany({
+    where: {
+      isAvailableForPurchase: true,
+    },
+  });
+};
+
+export default function ProductsPage() {
+  return (
+    <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"}>
+      <Suspense
+        fallback={
+          <>
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+          </>
+        }
+      >
+        <ProductsSuspense />
+      </Suspense>
+    </div>
+  );
+}
+
+async function ProductsSuspense() {
+  const products = await getProducts();
+
+  return products.map((product) => (
+    <ProductCard key={product.id} {...product} />
+  ));
+}
